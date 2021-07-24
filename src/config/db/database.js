372a62ddfa8d6dbbf7f494a613079d8d82f46e4b
TanTheVinh <tanthevinh@gmail.com>
2021-07-24 09:52:45 +0700

    add

--- src/config/db/database.js
+++ src/config/db/database.js
@@ -4,11 +4,7 @@ var pool = new Pool({
     host: 'localhost',
     database: 'mqtt',
     user: 'postgres',
-<<<<<<< HEAD
     password: 'sa',
-=======
-    password: '7714119',
->>>>>>> 34dbda849e4c52bee1f7e632e1d3ea79e22b7f74
     port: 5432
 });
 
