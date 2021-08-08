0d85add88ad9ef9b69ed15f005efa19a51ef3731
selqthang <selqthang@gmail.com>
2021-08-08 16:14:17 +0700

    edit

--- src/config/db/database.js
+++ src/config/db/database.js
@@ -4,11 +4,7 @@ var pool = new Pool({
     host: 'localhost',
     database: 'mqtt',
     user: 'postgres',
-<<<<<<< HEAD
-    password: 'sa',
-=======
     password: '7714119',
->>>>>>> 73a87e5d593d17139db25b5017aea735668e2321
     port: 5432
 });
 
